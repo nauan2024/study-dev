1fd4ad06b740208322a3f4b9b4ef9fadd7032fff
nauan2024 <nauanlaurentinotec@gmail.com>
2024-12-10 17:13:48 -0300

    feat: aula com o lucas

---
+++ Study-Dev/hoje/fundamtals.js
@@ -0,0 +1,23 @@
+//variables
+let variavel = 'sou alteravel'
+const constante = 'não é redeclaravel'
+variavel = 1
+//proibido reassinar uma constante
+// constante = 2
+
+//console.log(variavel)
+console.log(variavel)
+console.error(constante)
+console.info('sou uma informacao')
+console.warn('var é depreciado')
+
+const lastName ='correa'
+let age =25
+const person = {
+    name: 'joao',
+    age: 16,
+    gender: 'male'
+}
+const fruits = ['banana,', 'uva']
+
+console.table(fruits)
